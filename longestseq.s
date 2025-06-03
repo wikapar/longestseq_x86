5ddaa27661e9e836b3dac4ee667b017aab1c006a
wikapar <email@email>
2025-06-03 23:55:22 +0200

    tried fixing segfaulkt

--- longestseq.s
+++ longestseq.s
@@ -7,15 +7,15 @@ longestseq:
 
         push    edi
         push    esi
+        push    ebx
 
         sub     esp, 256        ;zarezerwuj 256 miejsca na stosie
-        mov     edx, esp        ;edx wskazuje na zarezerwowane miejsce
 
         mov     ecx, 256        ;ecx jako loop counter
         mov     ebx, [ebp+12]   ;wskazuje na wzorzec
 
 set_up_saving_space:
-        mov     byte [edx + ecx - 1], 0       ;zapisz 0 pod odpowiednim adresem
+        mov     byte [esp + ecx-1], 0       ;zapisz 0 pod odpowiednim adresem
         dec     ecx             ;dekrementacja petli -> samo ustawi flagi wiec nie trzeba test
         ;test    ecx, ecx
         jnz     set_up_saving_space
@@ -24,7 +24,7 @@ set_up_saving_space:
         mov     al, [ebx]       ;al ma znak z patterna
 save_pattern_on_stack:
         movzx   eax, al ;rozszerz al zerami do eax
-        mov     byte [edx + eax], 1      ;zapisz 1 na miejscu odpowiadajacym znakowi
+        mov     byte [esp + eax], 1      ;zapisz 1 na miejscu odpowiadajacym znakowi
         inc     ebx
 
         mov     al, [ebx]       ;al ma teraz przyszly znak
@@ -32,23 +32,19 @@ save_pattern_on_stack:
         jnz     save_pattern_on_stack     ;jesi al to nie zero kontunuuj
 
 set_up_finding_sequence:
-        sub     esp, 4  ;miejsce na pointer do poczatku najlepszej sekwencji
-        ;mov    cos, edx - 4
         mov     ebx, [ebp+8]    ;ebx wskazuje na string z argumentu
         mov     edi, ebx    ;edi bedzie wskazywac na poczatek aktualnej
-        mov     [edx - 4], ebx
+        xor     edx, edx        ;edx wskaznik na najdluzszy string
         xor     esi, esi        ;esi bedzie licznikiem aktualnej sekwencji
         xor     ecx, ecx        ;ecx bedzie licznikiem najdluzszej sekwencji
         ;al bedzie na znak czytany
-        ;edx jest na lookup table
-        ;edx - 4 -> pointer na najlepsza sekwencje
 
 next_char:
         mov     al, [ebx] ;al ma aktualny znak
         inc     ebx ;ebx wskazuje na nastepny znak
         movzx   eax, al ;rozszerz al zerami do eax
-        cmp     byte [edx + eax], 1
-        jne      not_in_pattern ;nie jest we wzorcu -> albo kontynuuj albo skoncz sekwencje jesli zaczeta
+        cmp     byte [esp + eax], 1
+        jne     not_in_pattern ;nie jest we wzorcu -> albo kontynuuj albo skoncz sekwencje jesli zaczeta
         ;jest we wzorcu
         test    esi, esi  ;czy esi to 0 -> jesli tak sekwencja nie zaczeta
         jnz     increase_len_counter
@@ -64,7 +60,7 @@ not_in_pattern:
 end_sequence:
         cmp     esi, ecx ;porownanie dlugosci curr_seq i najdluzszej sekwencji
         jle     clear_curr_sequence
-        mov     dword [edx - 4], edi ; zapisz wartosc z edi do edx - 4
+        mov     edx, edi ; zapisz wartosc z edi do edx
         mov     ecx, esi        ;do licznika najdluzszej sekwencji zapisz aktualna dlugosc
 
 clear_curr_sequence:
@@ -72,15 +68,15 @@ clear_curr_sequence:
         ;kiedy dojdzie do 0 to zawsze wejdzie tutaj i zakonczy sekwencje
 continue_loop:
         test    al, al  ;czy przeanalizowany znak to 0
-        jnz      next_char ;jesli nie zerto -> kontynuuj, jesli 0 -> idz do fin
+        jnz      next_char ;jesli nie zero -> kontynuuj, jesli 0 -> idz do fin
 
 fin:
-        mov     ebx, [edx - 4]  ;ebx to ptr na poczatek najdluzszej sekwencji
+        mov     ebx, [esp]  ;ebx to ptr na poczatek najdluzszej sekwencji
         add     ebx, ecx        ;dodaj dlugosc zeby otrzymac pointer na koniec stringa
         mov     byte [ebx], 0   ;zapisz 0 na koncu zwracanego stringa
-        mov     eax, [edx - 4]  ;do eax pointer na zwracany string
-        add     esp, 4
+        mov     eax, edx  ;do eax pointer na zwracany string
         add     esp, 256 ; zwolnienie miejsca na stosie
+        pop     ebx
         pop     esi
         pop     edi
         pop     ebp     ; przywroc ebp
